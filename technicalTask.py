7ef63b33f458f130641417f8789a87710bc6607f
uoa-ppat965 <68896686+uoa-ppat965@users.noreply.github.com>
2021-10-13 22:51:55 +1300

    Created the condition so it works for pr re

--- technicalTask.py
+++ technicalTask.py
@@ -4,8 +4,6 @@
 
 #Required packages
 import requests
-from requests.models import parse_url
-from requests.packages.urllib3.util import url
 
 print("Kia Ora, welcome to the to this program!")
 
@@ -18,14 +16,18 @@ query = "https://api.github.com/repos/" + owner + "/" + repo + "/pulls"
 print("Great sending a query for open Pull requests")
 r = requests.get(query) #goest to internet and grabs the stuff
 stuff =r.links 
-lastpageurl = str(stuff['last']['url'])
-setofUrl = lastpageurl.partition("=")
-maxpage = setofUrl[2]
 
-query = query + "?page=" + maxpage
-r = requests.get(query) #goes to internet and grabs the stuff
-otherpages = 30*(int(maxpage)-1)
-number = otherpages + len(r.json())
+if len(stuff) == 0:
+    number = len(r.json())
+else:
+    lastpageurl = str(stuff['last']['url'])
+    setofUrl = lastpageurl.partition("=")
+    maxpage = setofUrl[2]
+    query = query + "?page=" + maxpage
+    r = requests.get(query) #goes to internet and grabs the stuff
+    otherpages = 30*(int(maxpage)-1)
+    number = otherpages + len(r.json())
+    
 print(" ")
 print("Data Found! The number of open PRS is: ", number)
 print(" ")
