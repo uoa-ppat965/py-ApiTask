5197a556c3e3dfb6896ab69c80ce5d24c8a21886
uoa-ppat965 <68896686+uoa-ppat965@users.noreply.github.com>
2021-10-13 19:34:45 +1300

    Create technicalTask.py

---
+++ technicalTask.py
@@ -0,0 +1,17 @@
+#technical task for Multiudes
+#This code should use the github API extraction tool and takes the required info and print the number of pull requests
+#Author: uoa-ppat965
+
+print("Kia Ora, welcome to the to this program!")
+print("Let's process some Github data")
+
+owner = input("Who is the repo owner?")
+
+repo = input("What is the repo name")
+
+print("Great sending a query for open Pull requests")
+
+number = 0
+print("Data Found! The number of open PRS is: %d", number)
+
+print("Great! Bye for now")
